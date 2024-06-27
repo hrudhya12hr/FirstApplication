513d67e1d06dcce39d1e664ffa450b479bec598f
github1hrrr <hrudhya.sukumaran@gmail.com>
2024-06-27 23:28:11 +0530

    completed

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2;
+    AppCompatButton b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ed1=(EditText) findViewById(R.id.uname);
+        ed2=(EditText) findViewById(R.id.pass);
+        b1=(AppCompatButton) findViewById(R.id.log);
+        b2=(AppCompatButton) findViewById(R.id.gotoreg);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Reg button clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"WELCOME",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
